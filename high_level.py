f0ed4153388cad7044e8735bfb455c293d7f5d05
Shakthi Angou <75957659+ShakthiAngou@users.noreply.github.com>
2023-12-13 00:40:00 -0500

    Update high_level.py

--- high_level.py
+++ high_level.py
@@ -1,24 +1,20 @@
 from client import RedditClient
 
 def retrieve_most_upvoted_reply(api_client: RedditClient):
-    post_id = "your_post_id_here"
+    client.create_post("Sample Title", "Sample Text", "user123", "subreddit456")
+    
+    post_id = "1"
     limit = 5
 
-    # Retrieve the post content
     post_content = api_client.get_post_content(post_id)
     if post_content:
-        # Retrieve most upvoted comments under the post
         most_upvoted_comments = api_client.get_most_upvoted_comments(post_id, limit)
 
         if most_upvoted_comments.comments:
-            # Select the most upvoted comment
             most_upvoted_comment = most_upvoted_comments.comments[0]
-
-            # Expand the most upvoted comment
             expanded_comments = api_client.expand_comment_branch(most_upvoted_comment.comment_id, 2)
 
             if expanded_comments.comments:
-                # Find and return the most upvoted reply under the most upvoted comment
                 most_upvoted_reply = None
                 for comment in expanded_comments.comments:
                     if comment.parent_comment_id == most_upvoted_comment.comment_id:
@@ -26,7 +22,6 @@ def retrieve_most_upvoted_reply(api_client: RedditClient):
                             most_upvoted_reply = comment
 
                 return most_upvoted_reply
-
     return None
 
 # Example usage
